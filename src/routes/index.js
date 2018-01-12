5fdf9ef0d9c32d0d41011944ead3993133800051
yu <yuchiu2002@hotmail.com>
2018-01-12 10:55:50 -0500

    implemented brain wallet

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 const request = require('request')
-
+const bitcore = require('bitcore-lib')
 request({
     url: "https://blockchain.info/stats?format=json",
     json: true
@@ -9,16 +9,23 @@ request({
 
 module.exports = (app) => {
     app.get('/',
-        function (req, res) {
-            res.render("index", {
-                btcInfo: btcInfo
-            })
-        }
-    ),
-    app.post('/wallet',
-        function (req, res) {
-            var brainsrc = req.body.brainsrc
-            res.send(brainsrc)
-        }
-    )
+            function (req, res) {
+                res.render("index", {
+                    btcInfo: btcInfo
+                })
+            }
+        ),
+        app.post('/wallet',
+            function (req, res) {
+                const brainsrc = req.body.brainsrc
+
+                const input = new Buffer(brainsrc)
+                const hash = bitcore.crypto.Hash.sha256(input)
+                const bigNumber = bitcore.crypto.BN.fromBuffer(hash)
+                const privateKey = new bitcore.PrivateKey(bigNumber).toWIF()
+                const addy = new bitcore.PrivateKey(bigNumber).toAddress()
+                res.send("The Brain wallet of: " + brainsrc + "<br/>" +
+                    "The address/public key is: " + addy + "<br/>" + "Private key is: " + privateKey)
+            }
+        )
 }
